a78e9e272a38d9eafe2ab4c41da14daad86e42f7
Koaestro <koaestro@gmail.com>
2019-07-19 12:09:39 +0100

    Initial Commit

---
+++ autonomyBot.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const tokenfile = require("../tokens.json");
+const { CommandHandler } = require(`djs-commands`);
+const CH = new CommandHandler({
+
+    folder: __dirname + "/commands/",
+    prefix: ['>']
+
+});
+
+bot.on("ready", () => {
+  console.log("\nReady!");
+
+  bot.channels.find(channel => channel.name === "status").send(bot.user.username + " Connected!")
+});
+
+bot.on("message", (message) => {
+
+    if (message.author.bot) return;
+
+    let args = message.content.split(" ");
+    let command = args[0];
+
+    let cmd = CH.getCommand(command);
+
+    if (!cmd) return;
+
+    try{
+        cmd.run(bot, message, args.slice(1))
+    } catch(e){
+        console.log(e);
+    }
+
+})
+
+bot.login(tokenfile.AuTonomyBot);
---
+++ commands/info.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+module.exports = class info {
+
+    constructor(){
+        this.name = "info",
+        this.alias = "i",
+        this.usage = ">info"
+    }
+
+    run(bot, message, args) {
+
+        let msgEmbed = new Discord.RichEmbed()
+             .setDescription("AuTonomyBot Info")
+             .setColor("#1c0eb7")
+             .setThumbnail(bot.user.displayAvatarURL)
+             .addField("Name", bot.user.username)
+             .addField("Joined", bot.user.createdAt);
+
+        message.channel.send(msgEmbed);
+
+    }
+}
